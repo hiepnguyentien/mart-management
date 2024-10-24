39ff449683a6d83a1d5b000e7222adceea40f2a8
Nguyễn Tiến Hiệp <101116629+hiepnguyentien@users.noreply.github.com>
2024-10-24 16:47:49 +0700

    Refactor CustomerController to include info endpoint for retrieving customer profile

--- src/main/java/com/hiep/mart/controller/CustomerController.java
+++ src/main/java/com/hiep/mart/controller/CustomerController.java
@@ -2,15 +2,16 @@ package com.hiep.mart.controller;
 
 import com.hiep.mart.domain.dto.CustomerDTO;
 import com.hiep.mart.domain.request.CustomerRequest;
+import com.hiep.mart.domain.request.RegisterRequest;
 import com.hiep.mart.domain.response.ApiResponse;
 import com.hiep.mart.service.CustomerService;
+import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/customer")
@@ -18,6 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class CustomerController {
     CustomerService customerService;
+
+    @GetMapping("/info")
+    public ApiResponse<CustomerDTO> info(@Valid @RequestBody RegisterRequest registerRequest, @RequestParam(name = "lang", required = false) String lang) {
+        Locale locale = lang != null ? new Locale(lang) : Locale.getDefault();
+        ApiResponse<CustomerDTO> apiResponse = new ApiResponse<>();
+        apiResponse.setResult(customerService.viewProfile(locale));
+        return apiResponse;
+    }
 
     @PostMapping
     ApiResponse<CustomerDTO> addCustomer(@RequestBody CustomerRequest request) {
