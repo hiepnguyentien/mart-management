2d9ec4f291e47f9aa3a4d324f16ed27d3f800e59
Nguyễn Tiến Hiệp <101116629+hiepnguyentien@users.noreply.github.com>
2024-10-24 16:47:54 +0700

    Refactor ProductCartDTO to include promotional price calculation

--- src/main/java/com/hiep/mart/domain/dto/ProductCartDTO.java
+++ src/main/java/com/hiep/mart/domain/dto/ProductCartDTO.java
@@ -1,2 +1,30 @@
-package com.hiep.mart.domain.dto;public class ProductCartDTO {
+package com.hiep.mart.domain.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PUBLIC)
+public class ProductCartDTO {
+    Long productId;
+    String productName;
+    String productUnit;
+    String productImage;
+    String productStatus;
+    String productBrand;
+    Double productPrice;
+    Double discountPercentage;
+    Double discountAmount;
+    Double promotionalPrice;
+    Long quantity;
+    Long userId;
+
+    public Double getPromotionalPrice() {
+        if (getDiscountAmount() == null)
+            return getProductPrice();
+        return getProductPrice() - getProductPrice() * getDiscountAmount() / 100;
+    }
 }
