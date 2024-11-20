4be3d48c0b78f726a79ff6b5b6e5b48cd38d88a8
Nguyễn Tiến Hiệp <101116629+hiepnguyentien@users.noreply.github.com>
2024-11-20 10:39:23 +0700

    Add methods to retrieve expired products and products expiring in 7 days

--- src/main/java/com/hiep/mart/repository/ProductRepositoryCustom.java
+++ src/main/java/com/hiep/mart/repository/ProductRepositoryCustom.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface ProductRepositoryCustom {
     List<ProductDTO> getProductByCategoryId(Long categoryId);
     List<ProductDTO> getProductBySupplierId(Long supplierId);
+    List<ProductDTO> getExpiredProducts();
+    List<ProductDTO> getExpireIn7DaysProducts();
 }
--- src/main/java/com/hiep/mart/repository/impl/ProductRepositoryCustomImpl.java
+++ src/main/java/com/hiep/mart/repository/impl/ProductRepositoryCustomImpl.java
@@ -45,4 +45,36 @@ public class ProductRepositoryCustomImpl implements ProductRepositoryCustom {
 
         return query.getResultList();
     }
+
+    @Override
+    public List<ProductDTO> getExpiredProducts() {
+        StringBuilder sql = new StringBuilder("SELECT new com.hiep.mart.domain.dto.ProductDTO(\n" +
+                "    p.productId, p.productCode, p.productName, p.productPrice, p.productUnit, p.productDescription, \n" +
+                "    p.productImage, p.productStatus, p.productBrand, p.inventoryQuantity, p.suppliers.supplierId, \n" +
+                "    p.information, p2.discountPercentage, p2.discountAmount, p.promotionalPrice)\n" +
+                "FROM Products p\n" +
+                "LEFT JOIN p.promotions p2 \n" +
+                "WHERE p.suppliers.supplierId = :supplierId\n");
+
+        TypedQuery<ProductDTO> query = em.createQuery(sql.toString(), ProductDTO.class);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public List<ProductDTO> getExpireIn7DaysProducts() {
+        StringBuilder sql = new StringBuilder("SELECT new com.hiep.mart.domain.dto.ProductDTO(\n" +
+                "    p.productId, p.productCode, p.productName, p.productPrice, p.productUnit, p.productDescription, \n" +
+                "    p.productImage, p.productStatus, p.productBrand, p.inventoryQuantity, p.suppliers.supplierId, \n" +
+                "    p.information, p2.discountPercentage, p2.discountAmount, p.promotionalPrice)\n" +
+                "FROM Products p\n" +
+                "LEFT JOIN p.promotions p2 \n" +
+                "WHERE p.suppliers.supplierId = :supplierId\n");
+
+        TypedQuery<ProductDTO> query = em.createQuery(sql.toString(), ProductDTO.class);
+
+        return query.getResultList();
+    }
+
+
 }
