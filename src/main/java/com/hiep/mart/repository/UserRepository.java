53a039cdca3435d57cac17ee73333c406a1b5f83
Nguyễn Tiến Hiệp <101116629+hiepnguyentien@users.noreply.github.com>
2024-11-26 16:21:50 +0700

    Add custom query to UserRepository for retrieving customer ID by user ID

--- src/main/java/com/hiep/mart/repository/UserRepository.java
+++ src/main/java/com/hiep/mart/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.hiep.mart.repository;
 import com.hiep.mart.domain.dto.UserDTO;
 import com.hiep.mart.domain.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +12,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
     Optional<Users> findByUsername(String username);
+    @Query("SELECT u.customers.customerId FROM Users u WHERE u.userId = :userId")
+    Long findCustomerIdByUserId(@Param("userId") Long userId);
 }
